f19faf8078eb0fe9883e8627faaaa62e4f4764b0
Jacksona750 <jacksona750@gmail.com>
2019-03-11 19:39:10 -0700

    adding table reservation routes

--- server.js
+++ server.js
@@ -29,6 +29,24 @@ app.get("/reserve", function(req, res) {
     res.sendFile(path.join(__dirname, "reserve.html"));
 });
 
+// Displays all tables
+app.get("/api/tables", function(req, res) {
+    return res.json(tables);
+});
+
+// Desplays a single table, or returns false
+app.get("/api/tables/:table", function(req, res) {
+    var chosen = req.params.table;
+    console.log(chosen);
+
+    for (var i=0; i < tables.length; i++) {
+        if (chosen === tables[i].routeName) {
+            return res.json(tables[i]);
+        }
+    }
+    return res.json(false);
+})
+
 app.listen(PORT, function() {
     console.log("App listening on PORT " + PORT);
 });
