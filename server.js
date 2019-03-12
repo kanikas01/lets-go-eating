194ce6c294acad23b1294c8fef0bd137dcd44d2b
kanikas01 <larvakurt@yahoo.com>
2019-03-11 19:36:45 -0700

    Add table data

--- server.js
+++ server.js
@@ -12,7 +12,43 @@ var PORT = process.env.PORT || 3000;
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+// Table (DATA)
 // =============================================================
+
+var tables = [
+    {
+      name: 'Mr. Bill',
+      phone: '555-5555',
+      email: 'mrbill@snl.com',
+      id: 'BillyBoy'
+    },
+    {
+      name: 'Sluggo',
+      phone: '666-6666',
+      email: 'sluggo@snl.com',
+      id: 'GonnaGitBill'
+    },
+    {
+      name: 'Landshark',
+      phone: '777-7777',
+      email: 'landshark@snl.com',
+      id: 'ImADolphin'
+    },
+    {
+      name: 'Toonces',
+      phone: '888-8888',
+      email: 'toonces@snl.com',
+      id: 'IDriveGood'
+    },
+    {
+      name: 'Debbie Downer',
+      phone: '999-9999',
+      email: 'ddowner@snl.com',
+      id: 'LifeIsBad'
+    }
+];
+
 
 // Routes
 // =============================================================
