1686a953bd44724d49aa36a48f312ecda9c63d71
kanikas01 <larvakurt@yahoo.com>
2019-03-11 20:04:40 -0700

    Add post method to add table reservation

--- server.js
+++ server.js
@@ -87,7 +87,24 @@ app.get("/api/tables/:table", function(req, res) {
         }
     }
     return res.json(false);
-})
+});
+
+// Create New Characters - takes in JSON input
+app.post("/api/tables", function(req, res) {
+    // req.body hosts is equal to the JSON post sent from the user
+    // This works because of our body parsing middleware
+    var newTable = req.body;
+  
+    // Using a RegEx Pattern to remove spaces from newCharacter
+    // You can read more about RegEx Patterns later https://www.regexbuddy.com/regex.html
+    // newTable.routeName = newcharacter.name.replace(/\s+/g, "").toLowerCase();
+  
+    console.log(newTable);
+  
+    characters.push(newTable);
+  
+    res.json(newTable);
+  });
 
 app.listen(PORT, function() {
     console.log("App listening on PORT " + PORT);
